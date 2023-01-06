841db5f6eb406192f8c70619a6d4b52522cafa22
Rita Uzoma <you@example.com>
2023-01-06 10:16:00 +0100

    navbar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
 
 const Navbar = () => {
   return (
     <NavbarWrapper>
       <nav className="navigation-bar">
-        <div>Navbar</div>
         <div>
-          {/* <FontAwesomeIcon icon="fa-regular fa-moon" /> */}
-          <div>Navbar</div>
+          <h1 style={{ fontSize: "1.4rem" }}>Where in the world?</h1>
+        </div>
+        <div className="light-mode">
+          <FontAwesomeIcon icon={faMoon} />
+          <div>Dark Mode</div>
         </div>
       </nav>
     </NavbarWrapper>
@@ -20,13 +24,17 @@ export default Navbar;
 const NavbarWrapper = styled.nav`
   background: #fff;
   color: #000;
-
-  width: 100%;
-  padding: 15px 60px;
+  padding: 0px 65px;
   margin: 0;
 
-  .navigation-bar{
+  .navigation-bar {
     display: flex;
     justify-content: space-between;
+
+    .light-mode {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
   }
 `;
